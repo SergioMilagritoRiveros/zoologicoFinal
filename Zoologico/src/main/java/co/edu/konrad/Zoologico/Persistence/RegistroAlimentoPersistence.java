4c94c2a8ac19fba5bfdb85c1068d63cc516e2584
Sergio Riveros <sergioeriveros@gmaiol.com>
2019-04-06 14:54:34 -0500

    Se creo RegristroAlimentoPersistence

---
+++ Zoologico/src/main/java/co/edu/konrad/Zoologico/Persistence/RegistroAlimentoPersistence.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.konrad.Zoologico.Persistence;
+
+import co.edu.konrad.Zoologico.Entities.RegistroAlimentoEntity;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Sergio Riveros
+ */
+@Stateless
+public class RegistroAlimentoPersistence {
+        
+    /*
+    *manejador de entidad
+     */
+    @PersistenceContext(unitName = "zoologicoPU")
+    protected EntityManager em;
+
+    /*
+    *Buscar todas las entidades
+     */
+    public List<RegistroAlimentoEntity> encontrarTodos() {
+        Query todos = em.createQuery("select p from RegistroAlimentoEntity ");
+        return todos.getResultList();
+    }
+
+    /*
+    *buscar una entidad por su id
+     */
+    public RegistroAlimentoEntity encontrarPorId(Long id) {
+        return em.find(RegistroAlimentoEntity.class, id);
+    }
+
+    /*
+    *insertar un registro a la tabla 
+     */
+    public RegistroAlimentoEntity inserta(RegistroAlimentoEntity entity) {
+        em.persist(entity);
+        return entity;
+    }
+
+    /*
+    *actualizar los datos de una endtidad
+     */
+    public RegistroAlimentoEntity actualizar(RegistroAlimentoEntity entity) {
+        return em.merge(entity);
+    }
+
+    /*
+    *eliminar por id una entidad
+     */
+    public void eliminar(Long Id) {
+        RegistroAlimentoEntity productoEliminar = em.find(RegistroAlimentoEntity.class, Id);
+        em.remove(productoEliminar);
+    }  
+}
