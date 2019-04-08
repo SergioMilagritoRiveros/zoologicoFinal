406ad0a8fba52f202050e5dd60f7b0bfd9f18aed
Arturo Cortes <nelsonarturocortes@gmail.com>
2019-04-07 23:39:33 -0500

    se creo parte escencial del DTO tipo identificacion

--- Zoologico/src/main/java/co/edu/konrad/Zoologico/DTO/TipoIdentificacionDTO.java
+++ Zoologico/src/main/java/co/edu/konrad/Zoologico/DTO/TipoIdentificacionDTO.java
@@ -5,10 +5,36 @@
  */
 package co.edu.konrad.Zoologico.DTO;
 
+import co.edu.konrad.Zoologico.Entities.TipoIdentificacionEntity;
+
 /**
  *
  * @author ArturoC
  */
 public class TipoIdentificacionDTO {
+    private Long id;
+    private String TipoIdentificacion;
+
+    public TipoIdentificacionDTO(TipoIdentificacionEntity tip) {
+        this.id = tip.getId();
+        this.TipoIdentificacion = tip.getTipoIdentificacion();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTipoIdentificacion() {
+        return TipoIdentificacion;
+    }
+
+    public void setTipoIdentificacion(String TipoIdentificacion) {
+        this.TipoIdentificacion = TipoIdentificacion;
+    }
+    
     
 }
