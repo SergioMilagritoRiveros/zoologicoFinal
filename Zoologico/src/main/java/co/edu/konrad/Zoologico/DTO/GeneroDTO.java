62dd2a6648d9104871446930292f22b22f1e03a2
Arturo Cortes <nelsonarturocortes@gmail.com>
2019-04-07 23:38:18 -0500

    se creo parte escencial del DTO genero

--- Zoologico/src/main/java/co/edu/konrad/Zoologico/DTO/GeneroDTO.java
+++ Zoologico/src/main/java/co/edu/konrad/Zoologico/DTO/GeneroDTO.java
@@ -5,10 +5,36 @@
  */
 package co.edu.konrad.Zoologico.DTO;
 
+import co.edu.konrad.Zoologico.Entities.GeneroEntity;
+
 /**
  *
  * @author ArturoC
  */
 public class GeneroDTO {
+     private Long id;
+    private String Genero;
+
+    public GeneroDTO(GeneroEntity genero) {
+        this.id = genero.getId();
+        this.Genero = genero.getGenero();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGenero() {
+        return Genero;
+    }
+
+    public void setGenero(String Genero) {
+        this.Genero = Genero;
+    }
+    
     
 }
