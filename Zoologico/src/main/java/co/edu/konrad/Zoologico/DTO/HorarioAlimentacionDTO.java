41eb5141691af19258510acd841e6273a0c274e9
Arturo Cortes <nelsonarturocortes@gmail.com>
2019-04-07 23:38:32 -0500

    se creo parte escencial del DTO horarioAlimentacion

--- Zoologico/src/main/java/co/edu/konrad/Zoologico/DTO/HorarioAlimentacionDTO.java
+++ Zoologico/src/main/java/co/edu/konrad/Zoologico/DTO/HorarioAlimentacionDTO.java
@@ -5,10 +5,47 @@
  */
 package co.edu.konrad.Zoologico.DTO;
 
+import co.edu.konrad.Zoologico.Entities.HorarioAlimentacionEntity;
+import java.util.Date;
+
 /**
  *
  * @author ArturoC
  */
 public class HorarioAlimentacionDTO {
+    private Long id;
+    private Date horaDesayuno;
+    private Date horaComida;
+
+    public HorarioAlimentacionDTO(HorarioAlimentacionEntity hor) {
+        this.id = hor.getId();
+        this.horaDesayuno = hor.getHoraDesayuno();
+        this.horaComida = hor.getHoraComida();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getHoraDesayuno() {
+        return horaDesayuno;
+    }
+
+    public void setHoraDesayuno(Date horaDesayuno) {
+        this.horaDesayuno = horaDesayuno;
+    }
+
+    public Date getHoraComida() {
+        return horaComida;
+    }
+
+    public void setHoraComida(Date horaComida) {
+        this.horaComida = horaComida;
+    }
+    
     
 }
