79d46c36f2f513e56c309bda465d892b50b40794
Arturo Cortes <nelsonarturocortes@gmail.com>
2019-04-07 23:37:45 -0500

    se creo parte escencial del DTO animal

--- Zoologico/src/main/java/co/edu/konrad/Zoologico/DTO/AnimalDTO.java
+++ Zoologico/src/main/java/co/edu/konrad/Zoologico/DTO/AnimalDTO.java
@@ -5,10 +5,98 @@
  */
 package co.edu.konrad.Zoologico.DTO;
 
+import co.edu.konrad.Zoologico.Entities.*;
+
 /**
  *
  * @author ArturoC
  */
 public class AnimalDTO {
+    private Long AnimalID;
+    private TipoAnimalEntity tipoAnimalID;
+    private Long cantidadHabitad;
+    private Long cantidadTotal;
+    private HorarioAlimentacionEntity horarioAlimentacionID;
+    private EspacioEntity espaciosID;
+    private RatingEntity RatingID;
+    private EmpleadoEntity EmpleadoEntityID;
+
+    public AnimalDTO(AnimalEntity animal) {
+        this.AnimalID = animal.getId();
+        this.tipoAnimalID = animal.getAnimal();
+        this.cantidadHabitad = animal.getCantidadHabitad();
+        this.cantidadTotal = animal.getCantidadTotal();
+        this.horarioAlimentacionID = animal.getHorarioAlimentacionID();
+        this.espaciosID = animal.getEspaciosID();
+        this.RatingID = animal.getRatingID();
+        this.EmpleadoEntityID = animal.getEmpleadoEntityID();
+    }
+
+    public Long getAnimalID() {
+        return AnimalID;
+    }
+
+    public void setAnimalID(Long AnimalID) {
+        this.AnimalID = AnimalID;
+    }
+
+    public TipoAnimalEntity getTipoAnimalID() {
+        return tipoAnimalID;
+    }
+
+    public void setTipoAnimalID(TipoAnimalEntity tipoAnimalID) {
+        this.tipoAnimalID = tipoAnimalID;
+    }
+
+    public Long getCantidadHabitad() {
+        return cantidadHabitad;
+    }
+
+    public void setCantidadHabitad(Long cantidadHabitad) {
+        this.cantidadHabitad = cantidadHabitad;
+    }
+
+    public Long getCantidadTotal() {
+        return cantidadTotal;
+    }
+
+    public void setCantidadTotal(Long cantidadTotal) {
+        this.cantidadTotal = cantidadTotal;
+    }
+
+    public HorarioAlimentacionEntity getHorarioAlimentacionID() {
+        return horarioAlimentacionID;
+    }
+
+    public void setHorarioAlimentacionID(HorarioAlimentacionEntity horarioAlimentacionID) {
+        this.horarioAlimentacionID = horarioAlimentacionID;
+    }
+
+    public EspacioEntity getEspaciosID() {
+        return espaciosID;
+    }
+
+    public void setEspaciosID(EspacioEntity espaciosID) {
+        this.espaciosID = espaciosID;
+    }
+
+    public RatingEntity getRatingID() {
+        return RatingID;
+    }
+
+    public void setRatingID(RatingEntity RatingID) {
+        this.RatingID = RatingID;
+    }
+
+    public EmpleadoEntity getEmpleadoEntityID() {
+        return EmpleadoEntityID;
+    }
+
+    public void setEmpleadoEntityID(EmpleadoEntity EmpleadoEntityID) {
+        this.EmpleadoEntityID = EmpleadoEntityID;
+    }
+    
+    
+    
     
 }
