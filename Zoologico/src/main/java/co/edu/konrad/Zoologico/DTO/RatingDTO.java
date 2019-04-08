beb51a830e5c0b441556dd5a445b84f2774323a1
Arturo Cortes <nelsonarturocortes@gmail.com>
2019-04-07 23:38:39 -0500

    se creo parte escencial del DTO rating

--- Zoologico/src/main/java/co/edu/konrad/Zoologico/DTO/RatingDTO.java
+++ Zoologico/src/main/java/co/edu/konrad/Zoologico/DTO/RatingDTO.java
@@ -5,10 +5,46 @@
  */
 package co.edu.konrad.Zoologico.DTO;
 
+import co.edu.konrad.Zoologico.Entities.RatingEntity;
+
 /**
  *
  * @author ArturoC
  */
 public class RatingDTO {
+    private Long id;
+    private Long puntuacion;
+    private String comentario;
+
+    public RatingDTO(RatingEntity rating) {
+        this.id = rating.getId();
+        this.puntuacion = rating.getPuntuacion();
+        this.comentario = rating.getComentario();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(Long puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+    
     
 }
