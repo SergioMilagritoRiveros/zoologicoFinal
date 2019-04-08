fd642404ac94157e382c4b1b88365381998c41be
Arturo Cortes <nelsonarturocortes@gmail.com>
2019-04-07 23:39:25 -0500

    se creo parte escencial del DTO tipo espacio

--- Zoologico/src/main/java/co/edu/konrad/Zoologico/DTO/TipoEspacioDTO.java
+++ Zoologico/src/main/java/co/edu/konrad/Zoologico/DTO/TipoEspacioDTO.java
@@ -5,10 +5,56 @@
  */
 package co.edu.konrad.Zoologico.DTO;
 
+import co.edu.konrad.Zoologico.Entities.TipoEspacioEntity;
+
 /**
  *
  * @author ArturoC
  */
 public class TipoEspacioDTO {
+    private Long id;
+    private String nombreEspacio;
+    private String tamaño;
+    private Long dsponibilidadPersonas;
+
+    public TipoEspacioDTO(TipoEspacioEntity tip) {
+        this.id = tip.getId();
+        this.nombreEspacio = tip.getNombreEspacio();
+        this.tamaño = tip.getTamaño();
+        this.dsponibilidadPersonas = tip.getDsponibilidadPersonas();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombreEspacio() {
+        return nombreEspacio;
+    }
+
+    public void setNombreEspacio(String nombreEspacio) {
+        this.nombreEspacio = nombreEspacio;
+    }
+
+    public String getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(String tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    public Long getDsponibilidadPersonas() {
+        return dsponibilidadPersonas;
+    }
+
+    public void setDsponibilidadPersonas(Long dsponibilidadPersonas) {
+        this.dsponibilidadPersonas = dsponibilidadPersonas;
+    }
+    
     
 }
