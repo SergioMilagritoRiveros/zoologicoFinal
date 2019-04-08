371f525d219ab3fbf3e5c7b0aa55b45aa2d3e282
Arturo Cortes <nelsonarturocortes@gmail.com>
2019-04-07 23:38:53 -0500

    se creo parte escencial del DTO registro alimentacion

--- Zoologico/src/main/java/co/edu/konrad/Zoologico/DTO/RegistroAlimentoDTO.java
+++ Zoologico/src/main/java/co/edu/konrad/Zoologico/DTO/RegistroAlimentoDTO.java
@@ -5,10 +5,57 @@
  */
 package co.edu.konrad.Zoologico.DTO;
 
+import co.edu.konrad.Zoologico.Entities.RegistroAlimentoEntity;
+import co.edu.konrad.Zoologico.Entities.TipoAnimalEntity;
+
 /**
  *
 * @author ArturoC
  */
 public class RegistroAlimentoDTO {
+    private Long id;
+    private double cantidadAlimento;
+    private String TipoAlimento;
+    private TipoAnimalEntity tipoAnimalID;
+
+    public RegistroAlimentoDTO(RegistroAlimentoEntity reg) {
+        this.id = reg.getId();
+        this.cantidadAlimento = reg.getCantidadAlimento();
+        this.TipoAlimento = reg.getTipoAlimento();
+        this.tipoAnimalID = reg.getTipoAnimalID();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getCantidadAlimento() {
+        return cantidadAlimento;
+    }
+
+    public void setCantidadAlimento(double cantidadAlimento) {
+        this.cantidadAlimento = cantidadAlimento;
+    }
+
+    public String getTipoAlimento() {
+        return TipoAlimento;
+    }
+
+    public void setTipoAlimento(String TipoAlimento) {
+        this.TipoAlimento = TipoAlimento;
+    }
+
+    public TipoAnimalEntity getTipoAnimalID() {
+        return tipoAnimalID;
+    }
+
+    public void setTipoAnimalID(TipoAnimalEntity tipoAnimalID) {
+        this.tipoAnimalID = tipoAnimalID;
+    }
+    
     
 }
