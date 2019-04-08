c0dba64e69c35aa020abb63bab9d66390a727656
Arturo Cortes <nelsonarturocortes@gmail.com>
2019-04-07 23:39:42 -0500

    se creo parte escencial del DTO zoologico

--- Zoologico/src/main/java/co/edu/konrad/Zoologico/DTO/ZoologicoDTO.java
+++ Zoologico/src/main/java/co/edu/konrad/Zoologico/DTO/ZoologicoDTO.java
@@ -5,10 +5,56 @@
  */
 package co.edu.konrad.Zoologico.DTO;
 
+import co.edu.konrad.Zoologico.Entities.*;
+
 /**
  *
  * @author ArturoC
  */
 public class ZoologicoDTO {
+    private Long id;
+    private Long AsignacionEspaciosID;
+    private ShowEntity ShowID;
+    private AnimalEntity AnimalID;
+
+    public ZoologicoDTO(ZoologicoEntity zoo) {
+        this.id = zoo.getId();
+        this.AsignacionEspaciosID = zoo.getAsignacionEspaciosID();
+        this.ShowID = zoo.getShowID();
+        this.AnimalID = zoo.getAnimalID();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getAsignacionEspaciosID() {
+        return AsignacionEspaciosID;
+    }
+
+    public void setAsignacionEspaciosID(Long AsignacionEspaciosID) {
+        this.AsignacionEspaciosID = AsignacionEspaciosID;
+    }
+
+    public ShowEntity getShowID() {
+        return ShowID;
+    }
+
+    public void setShowID(ShowEntity ShowID) {
+        this.ShowID = ShowID;
+    }
+
+    public AnimalEntity getAnimalID() {
+        return AnimalID;
+    }
+
+    public void setAnimalID(AnimalEntity AnimalID) {
+        this.AnimalID = AnimalID;
+    }
+    
     
 }
