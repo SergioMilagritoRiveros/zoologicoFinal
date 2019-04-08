9f0b051d61c38cfb881c6d5b4c81ade8f02853f8
Arturo Cortes <nelsonarturocortes@gmail.com>
2019-04-07 23:39:06 -0500

    se creo parte escencial del DTO tipo animal

--- Zoologico/src/main/java/co/edu/konrad/Zoologico/DTO/TipoAnimalDTO.java
+++ Zoologico/src/main/java/co/edu/konrad/Zoologico/DTO/TipoAnimalDTO.java
@@ -5,10 +5,59 @@
  */
 package co.edu.konrad.Zoologico.DTO;
 
+import co.edu.konrad.Zoologico.Entities.*;
+import java.util.List;
+
 /**
  *
  * @author ArturoC
  */
 public class TipoAnimalDTO {
+    private Long id; 
+    private List<EspecieEntity> EspecieID;
+    private List<GeneroEntity> GeneroID;
+    private String Descripcion;
+
+    public TipoAnimalDTO(TipoAnimalEntity tip) {
+        this.id = tip.getId();
+        this.EspecieID = tip.getEspecieID();
+        this.GeneroID = tip.getGeneroID();
+        this.Descripcion = tip.getDescripcion();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<EspecieEntity> getEspecieID() {
+        return EspecieID;
+    }
+
+    public void setEspecieID(List<EspecieEntity> EspecieID) {
+        this.EspecieID = EspecieID;
+    }
+
+    public List<GeneroEntity> getGeneroID() {
+        return GeneroID;
+    }
+
+    public void setGeneroID(List<GeneroEntity> GeneroID) {
+        this.GeneroID = GeneroID;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+
+    
+    
     
 }
