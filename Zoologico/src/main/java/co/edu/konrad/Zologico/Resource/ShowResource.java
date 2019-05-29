c9547f7d68fa85cf5b76042c96be7220db281f0d
Arturo Cortes <nelsonarturocortes@gmail.com>
2019-05-28 23:06:13 -0500

    añadi los path faltantes

--- Zoologico/src/main/java/co/edu/konrad/Zologico/Resource/ShowResource.java
+++ Zoologico/src/main/java/co/edu/konrad/Zologico/Resource/ShowResource.java
@@ -11,17 +11,23 @@ import co.edu.konrad.Zoologico.Entities.ShowEntity;
 import co.edu.konrad.Zoologico.Logic.ShowLogic;
 import java.util.List;
 import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  *
  * @author ArturoC
  */
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Path("/Show")  
 public class ShowResource {
     @EJB
     private ShowLogic ShowLogic;
--- Zoologico/src/main/java/co/edu/konrad/Zologico/Resource/ZoologicoResource.java
+++ Zoologico/src/main/java/co/edu/konrad/Zologico/Resource/ZoologicoResource.java
@@ -11,18 +11,23 @@ import co.edu.konrad.Zoologico.Entities.ZoologicoEntity;
 import co.edu.konrad.Zoologico.Logic.ZoologicoLogic;
 import java.util.List;
 import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  *
  * @author ArturoC
  */
-
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Path("/Zoologico")  
 public class ZoologicoResource {
     @EJB
     private ZoologicoLogic ZoologicoLogic;
