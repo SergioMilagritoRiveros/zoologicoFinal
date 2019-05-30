bd7f596ffb345e5e86660e59e3564153995e5f54
julian tamayo <juliantamayo16@outlook.es>
2019-05-30 17:13:17 -0500

    Revert "registro de alimentacion"
    This reverts commit 9f6f7d69c906e92a59d0dd21dca05fea0ea81873.

--- ZoologicoView/src/app/services/Show/show.service.ts
+++ ZoologicoView/src/app/services/Show/show.service.ts
@@ -1,31 +1,9 @@
+import { Injectable } from '@angular/core';
 
-  import { Injectable } from '@angular/core';
-  import { HttpClient } from '@angular/common/http';
-  @Injectable({
-    providedIn: 'root'
-  })
-  export class ShowService {
-  
-    constructor(private http: HttpClient) { 
-  
-    }
-    show = '/Zoologico/api/Show';
-  
-  getShows() {
-    return this.http.get(this.show);
-  } 
-  getShowsid(id:number) {
-    return this.http.get(this.show+'/'+id);
-  }
-  deleteShow (id: number) {
-    
-    return this.http.delete(this.show+'/'+id);
-  }
-  postShow(id:number, show:string){
-    return this.http.post(this.show,{show,id});
-  }
-  putShow(id:number, show:string){
-    return this.http.put(this.show+'/'+id,{show,id});
-  }
-  }
-  
+@Injectable({
+  providedIn: 'root'
+})
+export class ShowService {
+
+  constructor() { }
+}
